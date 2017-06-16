8c9755b0141c1eeabffd7626b7899a012163e571
赖陆波 <lailb@gffunds.com.cn>
2017-06-16 19:28:39 +0800

    init file structure

---
+++ app/src/main/java/com/llb/joke/view_model/MainViewModel.java
@@ -0,0 +1,15 @@
+package com.llb.joke.view_model;
+
+import com.llb.joke.view_model.base.IViewModel;
+
+/**
+ * Created by Derrick on 2017/6/16.
+ */
+
+public class MainViewModel implements IViewModel {
+
+    @Override
+    public void destory() {
+
+    }
+}
---
+++ app/src/main/java/com/llb/joke/view_model/base/IViewModel.java
@@ -0,0 +1,9 @@
+package com.llb.joke.view_model.base;
+
+/**
+ * Created by Derrick on 2017/6/16.
+ */
+
+public interface IViewModel {
+    void destory();
+}
