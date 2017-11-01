e8d0625efb75ec4477f53e64f960b58b2c4b8acf
赖陆波 <lailb@gffunds.com.cn>
2017-11-01 17:37:01 +0800

    解决response zip deflate压缩解码问题

--- app/src/main/java/com/llb/subway/model/SubwayLoader.java
+++ app/src/main/java/com/llb/subway/model/SubwayLoader.java
@@ -1,15 +1,20 @@
 package com.llb.subway.model;
 
+
 import com.llb.subway.model.api.HttpUtil;
 import com.llb.subway.model.api.SubwayURL;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
 
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.Response;
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
-
 /**
  * Created by llb on 2017-09-06.
  */
@@ -34,7 +39,9 @@ public class SubwayLoader {
                                         .addHeader("User-Agent","Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1")
 //                    .addParameter("username", userName)
                                         .execute();
-                                return new String(res.body().bytes(), "utf-8");
+//                                return new String(res.body().bytes(), "utf-8");
+                                InputStream inputStream = new ByteArrayInputStream(res.body().bytes());
+                                return zipInputStream(inputStream);
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
@@ -42,6 +49,24 @@ public class SubwayLoader {
                         }
                 )
                 .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    /**
+     * 处理gzip,deflate返回流
+     *
+     * @param is
+     * @return
+     * @throws IOException
+     */
+    private String zipInputStream(InputStream is) throws IOException {
+        GZIPInputStream gzip = new GZIPInputStream(is);
+        BufferedReader in = new BufferedReader(new InputStreamReader(gzip, "utf-8"));
+        StringBuffer buffer = new StringBuffer();
+        String line;
+        while ((line = in.readLine()) != null)
+            buffer.append(line + "\n");
+        is.close();
+        return buffer.toString();
     }
 }
 
