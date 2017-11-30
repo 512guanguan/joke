4dd4c020555d514b69bb32ebeca6f73a9437e2cb
赖陆波 <lailb@gffunds.com.cn>
2017-11-30 19:55:50 +0800

    弹框工具类先拿来用着

---
+++ app/src/main/java/com/llb/subway/common/util/DialogUtil.java
@@ -0,0 +1,159 @@
+package com.llb.subway.common.util;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
+import android.text.Html;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.llb.subway.common.widget.CustomResultDialog;
+
+/**
+ * TODO 先用着有空待梳理
+ */
+public class DialogUtil {
+    /**
+     * 确认取消弹窗
+     */
+    public static void showDialog(String title, String msg, Context context, final DialogListener listener) {
+        showDialog(title, msg, null, null, context, listener);
+    }
+
+    /**
+     * @param title    标题
+     * @param msg      信息
+     * @param btn1     右边按钮
+     * @param btn2     左边按钮
+     * @param context
+     * @param listener 返回监听
+     */
+    public static void showDialog(String title, CharSequence msg, String btn1, String btn2, Context context,
+                                  final DialogListener listener) {
+        showDialog(title, msg, btn1, btn2, context, listener, true, View.INVISIBLE);
+    }
+
+    public static void showDialog(String title, CharSequence msg, String btn1, String btn2, Context context,
+                                  final DialogListener listener, boolean Canceled, int visibility) {
+        showDialog(title, msg, null, btn1, btn2, context, listener, Canceled, View.INVISIBLE);
+    }
+
+    /**
+     * @param title    标题
+     * @param msg      信息
+     * @param msg1     信息(字符串类型)
+     * @param btn1     右边按钮
+     * @param btn2     左边按钮
+     * @param context
+     * @param listener 返回监听
+     * @param Canceled 是否能点击屏幕消失
+     */
+    public static void showDialog(final String title, CharSequence msg, String msg1, String btn1, String btn2, final Context context,
+                                  final DialogListener listener, boolean Canceled, int visibility) {
+        final CustomResultDialog customResultDialog = new CustomResultDialog(context, 1);
+        customResultDialog.setTitle(title);
+        if (msg1 != null) {
+
+            customResultDialog.setNotic(msg1);
+        } else {
+            customResultDialog.setNotic(msg);
+        }
+        customResultDialog.setX(visibility);
+        if (btn1 == null) {
+            btn1 = "确认";
+        }
+        if (btn2 == null) {
+            btn2 = "取消";
+        }
+        customResultDialog.setBtnText(btn2, btn1);
+        customResultDialog.setOnPositiveListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                customResultDialog.dismiss();
+                listener.onNegativeTaskOver();
+            }
+        });
+        customResultDialog.setOnNegativeListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                customResultDialog.dismiss();
+                listener.onPositiveTaskOver();
+            }
+        });
+        customResultDialog.getWindow().setGravity(Gravity.CENTER);
+        customResultDialog.setCanceledOnTouchOutside(Canceled);
+        if (!Canceled) {// 如果不能点击屏幕消失，则把点击返回按钮时消失的功能去掉
+            customResultDialog.canback(false);
+        }
+        customResultDialog.getWindow().setLayout(android.view.WindowManager.LayoutParams.MATCH_PARENT,
+                android.view.WindowManager.LayoutParams.WRAP_CONTENT);
+        customResultDialog.show();
+        customResultDialog.setOnDismissListener(new OnDismissListener() {
+
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                listener.onDismiss();
+            }
+        });
+    }
+
+    public interface DialogListener {
+        void onPositiveTaskOver();
+
+        void onNegativeTaskOver();
+
+        void onDismiss();
+    }
+
+    public static void showDialog(String title, String msg, String btn1, Context context,
+                                  final DialogListener listener) {
+        showDialog(title, msg, btn1, context, listener, true);
+    }
+
+    //单个按钮
+    public static void showDialog(String title, String msg, String btn1, Context context,
+                                  final DialogListener listener, boolean Canceled) {
+        final CustomResultDialog customResultDialog = new CustomResultDialog(context, 1);
+        customResultDialog.setTitle(title);
+        if (msg != null && msg.contains("</font>")) {
+            customResultDialog.setNotic(Html.fromHtml(msg));
+        } else {
+            customResultDialog.setNotic(msg);
+        }
+        customResultDialog.setX(View.INVISIBLE);
+        if (btn1 == null) {
+            btn1 = "确 " + " " + " " + " " + "  认";
+        }
+        customResultDialog.setBtnText(null, btn1);
+        customResultDialog.setOnNegativeListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if (listener != null) {//因为单个按钮改了颜色，所以这里的监听反了
+                    listener.onPositiveTaskOver();
+                }
+                customResultDialog.dismiss();
+            }
+        });
+        customResultDialog.setCanceledOnTouchOutside(Canceled);
+        if (!Canceled) {// 如果不能点击屏幕消失，则把点击返回按钮时消失的功能去掉
+            customResultDialog.canback(false);
+        }
+        customResultDialog.getWindow().setGravity(Gravity.CENTER);
+        customResultDialog.getWindow().setLayout(android.view.WindowManager.LayoutParams.WRAP_CONTENT,
+                android.view.WindowManager.LayoutParams.WRAP_CONTENT);
+        customResultDialog.show();
+        customResultDialog.setOnDismissListener(new OnDismissListener() {
+
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                if (listener != null) {
+                    listener.onDismiss();
+                }
+            }
+        });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/llb/subway/common/widget/CustomResultDialog.java
@@ -0,0 +1,155 @@
+package com.llb.subway.common.widget;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.llb.subway.R;
+
+public class CustomResultDialog extends Dialog {
+    private TextView title_text, notic_text;
+    private Button confirmBtn, confirmBtn_1;
+    private View confirm_line;
+    private TextView closeTxt;
+    /**
+     * 标记弹框类型 4-高端理财购买的合格投资者认定
+     **/
+    private int id = 0;
+    private boolean canback = true;
+
+    public CustomResultDialog(Context mContext) {
+        super(mContext, R.style.CustomDialog);
+        setCustomDialog();
+    }
+
+    public CustomResultDialog(Context mContext, int id) {
+        super(mContext, R.style.CustomDialog);
+        this.id = id;
+        setCustomDialog();
+    }
+
+    public void canback(boolean canback) {
+        this.canback = canback;
+    }
+
+    @Override
+    public void onBackPressed() {
+        // TODO Auto-generated method stub
+        // System.out.println("onBackPressed");
+        if (canback) {
+            super.onBackPressed();
+        }
+    }
+
+    public void showD() {
+        getWindow().setGravity(Gravity.CENTER);
+        getWindow().setLayout(android.view.WindowManager.LayoutParams.FILL_PARENT,
+                android.view.WindowManager.LayoutParams.WRAP_CONTENT);
+        show();
+    }
+
+    private void setCustomDialog() {
+        View mView;
+        mView = LayoutInflater.from(getContext()).inflate(R.layout.custom_result_dialog, null);
+        confirmBtn = (Button) mView.findViewById(R.id.confirm_btn);
+        confirmBtn_1 = (Button) mView.findViewById(R.id.confirm_btn_1);
+        confirm_line = mView.findViewById(R.id.confirm_line);
+        title_text = (TextView) mView.findViewById(R.id.hint_txt);
+        notic_text = (TextView) mView.findViewById(R.id.notic_txt);
+        closeTxt = (TextView) mView.findViewById(R.id.close_txt);
+        if (id == 4) {
+            notic_text.getLayoutParams().height = android.view.WindowManager.LayoutParams.WRAP_CONTENT;// setHeight();
+            notic_text.setGravity(android.view.Gravity.LEFT);
+            closeTxt.setVisibility(View.GONE);
+        }
+        closeTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                dismiss();
+            }
+        });
+
+        super.setContentView(mView);
+    }
+
+    /**
+     * 确定键监听器
+     *
+     * @param listener
+     */
+    public void setOnPositiveListener(View.OnClickListener listener) {
+        confirmBtn.setOnClickListener(listener);
+    }
+
+    public void setOnNegativeListener(View.OnClickListener listener) {
+        confirmBtn_1.setOnClickListener(listener);
+    }
+
+    public void setTitle(CharSequence title) {
+        if (title != null) {
+            title_text.setText(title);
+        } else {
+            title_text.setVisibility(View.GONE);
+        }
+    }
+
+    public void setNotic(String notic) {
+        if (notic != null) {
+            notic = notic.replace("\\n", "\n");
+            notic_text.setGravity(Gravity.LEFT);
+            notic_text.setText(notic);
+        } else {
+            notic_text.setVisibility(View.GONE);
+        }
+    }
+
+    public void setNotic(CharSequence notic) {
+        if (notic != null) {
+            notic_text.setText(notic);
+        } else {
+            notic_text.setVisibility(View.GONE);
+        }
+    }
+
+    public void setX() {
+        closeTxt.setVisibility(View.GONE);
+    }
+
+    public void setX(int visibility) {
+        closeTxt.setVisibility(visibility);
+    }
+
+    public void setBtnText(String btn1, String btn2) {
+        if (btn1 != null) {
+            confirmBtn.setText(btn1);
+        } else {
+            if (confirm_line != null){
+                confirm_line.setVisibility(View.GONE);
+            }
+            confirmBtn.setVisibility(View.GONE);
+        }
+        if (btn2 != null) {
+            confirmBtn_1.setText(btn2);
+        } else {
+            if (confirm_line != null){
+                confirm_line.setVisibility(View.GONE);
+            }
+            confirmBtn_1.setVisibility(View.GONE);
+        }
+    }
+
+    public void setBtnText(String btn1) {
+
+        confirmBtn_1.setText(btn1);
+
+    }
+
+    public void setBtnClible(boolean btn1, boolean btn2) {
+        confirmBtn.setEnabled(btn1);
+        confirmBtn_1.setEnabled(btn2);
+    }
+
+}
